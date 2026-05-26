750644ee7761bf7a36bb50a85ae4f9500c8de659
Durgesh Bhatt <151530880+durgesh-5699@users.noreply.github.com>
2026-05-26 17:13:54 +0530

    Time: 4 ms (17.39%) | Memory: 11.1 MB (12.74%) - LeetSync

---
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0) return 0;
+        if(n==1) return 1;
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        dp[1][0]=1;
+        dp[1][1]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<i;j++){
+                if(nums[j-1]>nums[i-1]){
+                    dp[i][0] = max(dp[i][0],dp[j][1]+1);
+                }else if(nums[j-1]<nums[i-1]){
+                    dp[i][1] = max(dp[i][1],dp[j][0]+1);
+                }
+            }
+        }
+        return max({1,dp[n][0],dp[n][1]});
+    }
+};
