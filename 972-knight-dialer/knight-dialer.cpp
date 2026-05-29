64dfdec7096f9d3e4a6283dcb15c95aa772eae06
Durgesh Bhatt <151530880+durgesh-5699@users.noreply.github.com>
2026-05-29 17:04:38 +0530

    Time: 48 ms (80.32%) | Memory: 11 MB (72.90%) - LeetSync

---
+++ 972-knight-dialer/knight-dialer.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    const int mod = 1e9+7;
+    int dp[50001][10];
+    vector<vector<int>>v = {
+            {4,6},
+            {6,8},
+            {7,9},
+            {4,8},
+            {0,3,9},
+            {},
+            {0,1,7},
+            {2,6},
+            {1,3},
+            {2,4}
+        };
+    int solve(int n,int cell){
+        if(n==0) return 1;
+        if(dp[n][cell]!=-1){
+            return dp[n][cell];
+        }
+        int ans=0;
+        for(int neigh : v[cell]){
+            ans = (ans+solve(n-1,neigh))%mod;
+        }
+        return dp[n][cell]=ans;
+    }
+    int knightDialer(int n){
+        int ans=0;
+        memset(dp,-1,sizeof(dp));
+        for(int cell=0;cell<=9;cell++){
+            ans = (ans + solve(n-1,cell))%mod;
+        }
+        return ans;
+    }
+};
